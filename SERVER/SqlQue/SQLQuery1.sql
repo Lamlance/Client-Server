61efb3548d4c61694003e604d348986ea1c3cf78
Hoang Lam <lamhoangdien113@gmail.com>
2021-11-30 14:10:56 +0700

    Add update databases

---
+++ SERVER/SqlQue/SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿DECLARE @seachID AS INT
+SELECT @seachID = 1
+
+SELECT PBook.Serial_Number, PBook.Full_Name, PBook.Phone_Number, PBook.Email,
+PService.ServiceName, Comp.CompanyName,PJob.Note
+FROM PhoneBook AS PBook
+
+INNER JOIN PersonalJob AS PJob
+ON PBook.Id = PJob.PhoneBookId
+
+INNER JOIN Company AS Comp
+ON Comp.Id = PJob.CompanyId
+
+INNER JOIN ProvidedServices AS PService
+ON PService.Id = PJob.ServiceId
+
+WHERE PBook.Id = @seachID
